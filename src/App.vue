e9ccd20bf9a4a5f4caa3b78c50eec774ebf93867
gonglei <gonglei@wsecar.com>
2020-12-17 19:07:03 +0800

    调整样式

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,50 @@
 <template>
 	<div id="nav">
-		<router-link to="/">Home</router-link> |
-		<router-link to="/about">About</router-link>
+		<van-tabbar v-model="active">
+			<van-tabbar-item
+				v-for="item in tabList"
+				:icon="item.icon"
+				:key="item"
+				>{{ item.label }}
+			</van-tabbar-item>
+			<!-- <van-tabbar-item icon="search">标签</van-tabbar-item>
+			<van-tabbar-item icon="friends-o">标签</van-tabbar-item>
+			<van-tabbar-item icon="setting-o">标签</van-tabbar-item> -->
+		</van-tabbar>
 	</div>
 	<router-view />
 </template>
-<style lang="scss">
-#app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
+<script lang="ts">
+import { ref } from 'vue'
+export default {
+	setup() {
+		const tabList = ref([
+			{
+				label: '首页',
+				icon: 'home-o',
+			},
+			{
+				label: '购物车',
+				icon: 'shopping-cart-o',
+			},
+			{
+				label: '我的',
+				icon: 'friends-o',
+			},
+		])
+		const active = ref(0)
+		return { tabList, active }
+	},
 }
-
-#nav {
-	padding: px2rem(30);
-
-	a {
-		font-weight: bold;
-		color: #2c3e50;
-
-		&.router-link-exact-active {
-			color: #42b983;
-		}
-	}
+</script>
+<style lang="scss" scoped>
+@import '@/assets/styles/hotcss/px2rem.scss';
+::v-deep .van-tabbar-item {
+	font-size: px2rem(24);
+	// line-height: 2;
+}
+::v-deep .van-tabbar {
+	height: px2rem(80);
+	padding: px2rem(5) 0;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/assets/styles/hotcss/hotcss.js' // 自定义 css
-
 import '@/assets/styles/reset.css' // 自定义 css
 
+import Vant from 'vant';
+import 'vant/lib/index.css';
 
 
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(Vant).mount('#app')
