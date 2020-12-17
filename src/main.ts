362c2e597893b3e5b8aa72ef912bb22bddda9dfb
gonglei <gonglei@wsecar.com>
2020-12-17 17:13:54 +0800

    develop

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@/assets/styles/hotcss/hotcss.js' // 自定义 css
+
+import '@/assets/styles/reset.css' // 自定义 css
+
+
+
 
 createApp(App).use(store).use(router).mount('#app')
