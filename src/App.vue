563fec3cde392af6dba3752872f5379684c0007e
gonglei <gonglei@wsecar.com>
2020-12-18 09:39:12 +0800

    配置路由跳转

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
 	<div id="nav">
-		<van-tabbar v-model="active">
+		<van-tabbar v-model="active" @change="handlerChange">
 			<van-tabbar-item
 				v-for="item in tabList"
 				:icon="item.icon"
 				:key="item"
-                :badge="item.badge"
+				:badge="item.badge"
 				>{{ item.label }}
 			</van-tabbar-item>
 		</van-tabbar>
@@ -14,28 +14,32 @@
 </template>
 <script lang="ts">
 import { ref } from 'vue'
+import router from '@/router'
 export default {
 	setup() {
 		const tabList = ref([
 			{
 				label: '首页',
-				name: '',
+				name: 'home',
 				icon: 'home-o',
 			},
 			{
 				label: '购物车',
-				name: '',
-                icon: 'shopping-cart-o',
-                badge:"5"
+				name: 'car',
+				icon: 'shopping-cart-o',
+				badge: '5',
 			},
 			{
 				label: '我的',
-				name: '',
+				name: 'me',
 				icon: 'friends-o',
 			},
 		])
 		const active = ref(0)
-		return { tabList, active }
+		const handlerChange = (index: string) => {
+			router.push({ name: tabList.value[index].name })
+		}
+		return { tabList, active, handlerChange }
 	},
 }
 </script>
---
+++ src/views/car/Index.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="car">
+        <h2>购物车</h2>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+	name: 'Home',
+	components: {},
+	setup() {
+		const searchValue = ref('')
+		return { searchValue }
+	},
+})
+</script>
+<style lang="scss" scoped>
+@import '@/assets/styles/hotcss/px2rem.scss';
+.home {
+	padding: 0 px2rem(10);
+}
+</style>
